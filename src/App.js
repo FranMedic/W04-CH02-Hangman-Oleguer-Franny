a4cb02cb579dc9c7c54457bf5222938be65639a0
FranMedic <m.francisca.medico@gmail.com>
2021-10-19 19:35:22 +0200

    added word array to app and pass to LetterBox his props

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 import "./App.css";
+import LetterBox from "./components/LetterBox/LetterBox";
+import { useState } from "react";
 
 function App() {
-  return <></>;
+  const [word, setWord] = useState([
+    { letter: "h", guessed: true },
+    { letter: "o", guessed: false },
+    { letter: "l", guessed: true },
+    { letter: "a", guessed: true },
+  ]);
+  return (
+    <>
+      <div class="word">
+        <ul class="word__list">
+          {word.map((letter) => (
+            <LetterBox letter={letter.letter} guessed={letter.guessed} />
+          ))}
+        </ul>
+      </div>
+    </>
+  );
 }
 
 export default App;
