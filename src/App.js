01995f66b94fe146b65b725d82b4e83882ddcd5e
Oleguer Carreras <oleguer.6.bdn@hotmail.com>
2021-10-19 20:16:44 +0200

    Added linesSVG to print the svg lines

--- src/App.js
+++ src/App.js
@@ -4,26 +4,29 @@ import HangmanPicture from "./components/HangmanPicture/HangmanPicture";
 
 function App() {
   const linesSVG = [
-    <line class="stage11" x1="62" y1="70" x2="56" y2="56"></line>,
-    <line class="stage10" x1="50" y1="70" x2="56" y2="56"></line>,
-    <line class="stage9" x1="68" y1="46" x2="56" y2="40"></line>,
-    <line class="stage8" x1="44" y1="46" x2="56" y2="40"></line>,
-    <line class="stage7" x1="56" y1="40" x2="56" y2="56"></line>,
-    <circle class="stage6" cx="56" cy="34" r="6" fill="#bee5eb"></circle>,
-    <line class="stage5" x1="56" y1="16" x2="56" y2="28"></line>,
-    <line class="stage4" x1="24" y1="24" x2="32" y2="16"></line>,
-    <line class="stage3" x1="21" y1="16" x2="60" y2="16"></line>,
-    <line class="stage2" x1="24" y1="80" x2="24" y2="16"></line>,
     <line class="stage1" x1="16" y1="80" x2="32" y2="80"></line>,
+    <line class="stage2" x1="24" y1="80" x2="24" y2="16"></line>,
+    <line class="stage3" x1="21" y1="16" x2="60" y2="16"></line>,
+    <line class="stage4" x1="24" y1="24" x2="32" y2="16"></line>,
+    <line class="stage5" x1="56" y1="16" x2="56" y2="28"></line>,
+    <circle class="stage6" cx="56" cy="34" r="6" fill="#bee5eb"></circle>,
+    <line class="stage7" x1="56" y1="40" x2="56" y2="56"></line>,
+    <line class="stage8" x1="44" y1="46" x2="56" y2="40"></line>,
+    <line class="stage9" x1="68" y1="46" x2="56" y2="40"></line>,
+    <line class="stage10" x1="50" y1="70" x2="56" y2="56"></line>,
+    <line class="stage11" x1="62" y1="70" x2="56" y2="56"></line>,
   ];
 
-  const [wrongLetters, setWrongLetters] = useState(["A", "B"]);
+  const [wrongLetters, setWrongLetters] = useState(["A", "B", "k", "p"]);
 
   return (
     <>
-      <div class="svg">
-        <svg class="hangman" viewBox="0 0 96 96" width="300" height="300">
-          <HangmanPicture linesSVG={linesSVG.slice(0, wrongLetters.length)} />
+      <div className="svg">
+        <svg className="hangman" viewBox="0 0 96 96" width="300" height="300">
+          <HangmanPicture
+            linesSVG={linesSVG}
+            numberWrongLetters={wrongLetters.length}
+          />
         </svg>
       </div>
     </>
--- src/components/HangmanPicture/HangmanPicture.js
+++ src/components/HangmanPicture/HangmanPicture.js
@@ -1,7 +1,6 @@
-function HangmanPicture({ linesSVG }) {
-  return linesSVG.map(({ className, x1, y1, x2, y2 }) => (
-    <line class={className} x1={x1} y1={y1} x2={x2} y2={y2}></line>
-  ));
+function HangmanPicture({ linesSVG, numberWrongLetters }) {
+  const wrongLines = linesSVG.slice(0, numberWrongLetters);
+  return wrongLines;
 }
 
 export default HangmanPicture;
