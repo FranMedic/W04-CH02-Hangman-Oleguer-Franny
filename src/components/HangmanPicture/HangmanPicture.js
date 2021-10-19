ae7c30be3b6edf49a65dcec633c76c422e8493e8
FranMedic <m.francisca.medico@gmail.com>
2021-10-19 22:21:26 +0200

    added propTypes on HangmanPicture

--- src/components/HangmanPicture/HangmanPicture.js
+++ src/components/HangmanPicture/HangmanPicture.js
@@ -1,6 +1,11 @@
+import PropTypes from "prop-types";
+
 function HangmanPicture({ linesSVG, numberWrongLetters }) {
   const wrongLines = linesSVG.slice(0, numberWrongLetters);
   return wrongLines;
 }
-
+HangmanPicture.propTypes = {
+  linesSVG: PropTypes.array.isRequired,
+  numberWrongLetters: PropTypes.number.isRequired,
+};
 export default HangmanPicture;
