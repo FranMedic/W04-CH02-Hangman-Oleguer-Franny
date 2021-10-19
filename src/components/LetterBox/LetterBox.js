2b8e2b8b0488b2a8c9798aea27be770ab7f98bc8
FranMedic <m.francisca.medico@gmail.com>
2021-10-19 22:25:34 +0200

    added propTypes of letterbox

--- src/components/LetterBox/LetterBox.js
+++ src/components/LetterBox/LetterBox.js
@@ -1,5 +1,11 @@
+import PropTypes from "prop-types";
+
 function LetterBox({ wrongLetters }) {
   return wrongLetters.map((letter, index) => <p key={index}>{letter}</p>);
 }
 
 export default LetterBox;
+
+LetterBox.propTypes = {
+  wrongLetters: PropTypes.array,
+};
